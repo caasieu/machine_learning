f5019a6b1b324e4d65690e02e7e7ccd92ebc7078
Caasi2 <caasim353@gmail.com>
2024-06-06 17:50:46 -0300

    first migration

---
+++ env_commands.md
@@ -0,0 +1,9 @@
+# libs
+pip install pandas
+pip install -U scikit-learn
+pip install numpy 
+
+
+python3 -m pip show scikit-learn  # show scikit-learn version and location
+python3 -m pip freeze             # show all installed packages in the environment
+python3 -c "import sklearn; sklearn.show_versions()"
---
+++ movie_recommendation/movie_recommendation.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+#with open('./data/movies.csv', 'rb') as f:
+#  movies = f.read()
+
+movies = pd.read_csv('./data/movies.csv')
+ratings = pd.read_csv('./data/ratings.csv')  
+
+# Removing duplicate rows
+movies.drop_duplicates(inplace=True)
+ratings.drop_duplicates(inplace=True)
+
+# removing missing values
+movies.dropna(inplace=True)
+ratings.dropna(inplace=True)
+
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.neighbors import NearestNeighbors # Using Nearest Class from scikit-learn 
+
+def process_data():
+  
+  genres = movies['genres'] # feature (column) to be based on
+  encoder = OneHotEncoder() # instanciate the encoder
+  genres_encoded = encoder.fit_transform(genres.values.reshape(-1, 1)) # fitting and transforming the generes column
+  
+  #print('genres:', genres)
+  #print('genres_encoded:', genres_encoded)
+  
+  recommender = NearestNeighbors(metric='cosine') # we're using the cosine metric 
+  recommender.fit(genres_encoded.toarray())
+  
+  return recommender, genres_encoded
+    
+def recommend_movie(movie_index):
+  recommender, genres_encoded = process_data()
+  
+  #movie_index = index # the movie the user has previously watched
+  num_recommendations = 15 # number of recommendations to return
+  
+  # getting the recommendations
+  _, recommendations = recommender.kneighbors(genres_encoded[movie_index].toarray(), n_neighbors=num_recommendations)
+
+  # extracting the movie titles from the recommendations
+  recommend_movie_titles = movies.iloc[recommendations[0]] #['title']
+  
+    
+  print('movie_based_on:', movies.iloc[movie_index])
+  print('\nrecommendations:',recommend_movie_titles)
+
+recommend_movie(0)  
+  
+
+#print(movies)
+#print(ratings)
